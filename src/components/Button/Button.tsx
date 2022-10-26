64037c9ac1887490003db160ac0cde7f06818442
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-10-26 13:04:05 -0500

    botón con efecto hover

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import './styles/Button.css';
-export interface ButtonInterface {}
+import React from "react";
+import tecIcon from "../../assets/tec-icon.svg";
+import "./styles/Button.css";
 
-const Button : React.FC<ButtonInterface> = () => {
-	return <div className='button'>Button</div >;
+export interface ButtonInterface {
+  text: string;
+}
+
+const Button: React.FC<ButtonInterface> = ({ text }) => {
+  return (
+    <button className="button">
+      {text}
+      <img src={tecIcon} alt="tec-icon" className="button__icon" />
+    </button>
+  );
 };
 
 export default Button;
