8ad92b63a938c922232c16e914cba8fed5c1d28e
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-11-04 12:13:17 -0500

    importaciones sin usar removidas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Header } from "../../components/Header";
-import { Navbar } from "../../components/Navbar";
 import "./styles/Home.css";
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
   return (
     <div className="home">
-      {/* <Navbar theme="light"/> */}
       <Header />
       <div className="themes" style={{ height: '100vh' }}></div>
     </div>
