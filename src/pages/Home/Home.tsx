df6c8d39c0f43e935f7c6f7000dff0c9f92963a9
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-11-04 22:31:53 -0500

    botones de los temas agregados

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import "./styles/Home.css";
+import { FaMicrochip, FaBookReader, FaVideo } from "react-icons/fa";
+import { HiBeaker } from "react-icons/hi";
+import { ImBinoculars } from "react-icons/im";
 export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
   return (
     <div className="home">
       <Header />
-      <div className="themes" style={{ height: '100vh' }}></div>
+      <div className="themes">
+        <h1>Temas</h1>
+
+        <div className="themes__container">
+          <Button text="Tecnología" icon={<FaMicrochip />} color="blue" />
+          <Button text="Politica" icon={<FaBookReader />} color="orange" />
+          <Button text="Ciencia" icon={<HiBeaker />} color="green" />
+          <Button text="Cine y Arte" icon={<FaVideo />} color="lightPink" />
+          <Button text="Curiosidades" icon={<ImBinoculars />} color="darkPink" />
+        </div>
+      </div>
     </div>
   );
 };
