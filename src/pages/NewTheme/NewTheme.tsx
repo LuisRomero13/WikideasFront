7e64e57d3efc1103f5d27093c5392b8ced298025
Gian Diaz <49536327+GianDiazArce@users.noreply.github.com>
2022-11-15 16:00:45 -0500

    formulario de creacion de tema controlado con useState

--- src/pages/NewTheme/NewTheme.tsx
+++ src/pages/NewTheme/NewTheme.tsx
@@ -1,24 +1,50 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./styles/NewTheme.css";
 import { Editor } from "@tinymce/tinymce-react";
 export interface NewThemeInterface {}
 
 const NewTheme: React.FC<NewThemeInterface> = () => {
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {};
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("tecnologia");
+  const [content, setContent] = useState("");
+
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log({ title, category, content });
+  };
   const editorRef = useRef(null);
   return (
     <div className="newtheme">
       <h1 className="newtheme__title">Creación del tema</h1>
       <form className="form__container" onSubmit={submitHandler}>
-        <div style={{display: 'flex', width: '100%', alignItems: 'center', marginTop: '2em'}}>
-          <div className="form__content" >
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            alignItems: "center",
+            marginTop: "2em",
+          }}
+        >
+          <div className="form__content">
             <div className="form__row">
               <div className="form__group">
-                <input type="text" name="titulo" className="form__input" />
+                <input
+                  type="text"
+                  name="titulo"
+                  className="form__input"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                />
                 <label className="form__label">Titulo</label>
               </div>
               <div className="form__group">
-                <select name="category" id="category" className="form__select">
+                <select
+                  name="category"
+                  id="category"
+                  className="form__select"
+                  onChange={(e) => setCategory(e.target.value)}
+                  value={category}
+                >
                   <option value="tecnologia">Tecnologia</option>
                   <option value="politica">Politica</option>
                   <option value="ciencia">Ciencia</option>
@@ -31,7 +57,9 @@ const NewTheme: React.FC<NewThemeInterface> = () => {
             <Editor
               apiKey="bwpwtl3ji8ktvhywiu3rafc8sk7exeaj6axiww1b0fvyjlim"
               onInit={(evt, editor) => (editorRef.current = editor as any)}
-              initialValue="<p>This is the initial content of the editor.</p>"
+              initialValue={content}
+              value={content}
+              onChange={(e) => setContent(e.target.getContent())}
               init={{
                 height: 400,
                 menubar: false,
@@ -59,13 +87,18 @@ const NewTheme: React.FC<NewThemeInterface> = () => {
                   "undo redo | blocks | " +
                   "bold italic forecolor | alignleft aligncenter " +
                   "alignright alignjustify | bullist numlist outdent indent | " +
-                  "removeformat | help",
+                  "removeformat | ",
                 content_style:
                   "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+                language: "es_MX",
+                language_url: "/langs/es_MX.js",
               }}
             />
           </div>
-          <div className="form__upload" style={{display: 'grid', placeItems: 'center', width: '50%'}}>
+          <div
+            className="form__upload"
+            style={{ display: "grid", placeItems: "center", width: "50%" }}
+          >
             <label htmlFor="file">Sube una imagen</label>
             <input
               type="file"
@@ -76,19 +109,14 @@ const NewTheme: React.FC<NewThemeInterface> = () => {
             />
           </div>
         </div>
-        <button className="form__submit" type="submit">crear tema</button>
+        <button
+          className="form__submit"
+          type="submit"
+          disabled={content === "" || title === "" || category === ""}
+        >
+          crear tema
+        </button>
       </form>
-      {/* <div className="newtheme__texts">
-        <input type="text" className="newtheme__input" />
-      </div>
-      <div className="newtheme__upload">
-        <h1 className="newtheme__upload-title">Sube una imagen</h1>
-        <input
-          type="file"
-          className="newtheme__upload-input"
-          accept="image/*"
-        />
-      </div> */}
     </div>
   );
 };
